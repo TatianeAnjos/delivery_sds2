a2059d727ae791e785d56af0fb9506b78a72b18f
Tatiane Anjos <tatianeanjos1997@gmail.com>
2021-01-05 19:37:52 -0300

    Project created

---
+++ backend/src/test/java/com/tatiane/dsDelivery/DsDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tatiane.dsDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
